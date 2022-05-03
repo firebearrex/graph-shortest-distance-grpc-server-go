3f4d9666bcb1566ead485a0a7e1c6761311875cb
firebearrex <60494405+firebearrex@users.noreply.github.com>
2022-05-03 02:44:05 -0700

    Update post.go

--- graph_shortest_distance/server/post.go
+++ graph_shortest_distance/server/post.go
@@ -7,12 +7,12 @@ import (
 	pb "github.com/firebearrex/graph-shortest-distance-grpc-server-go/graph_shortest_distance/proto"
 )
 
-func (*Server) Post(ctx context.Context, in *pb.PostRequest) (*pb.PostResponse, error) {
-	log.Printf("Post was invoked with %v\n", in)
+func (*Server) Post(ctx context.Context, req *pb.PostRequest) (*pb.PostResponse, error) {
+	log.Printf("Post was invoked with %v\n", req)
 
 	newGraph := Graph{
-		totalVertices: in.TotalVertices,
-		edges:         in.Edges,
+		totalVertices: req.TotalVertices,
+		edges:         req.Edges,
 	}
 	currId := idHead
 	graphStore[idHead] = newGraph
