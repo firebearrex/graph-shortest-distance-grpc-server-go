e8e4c557fcc648abc9093850909cbd3e782ec561
firebearrex <60494405+firebearrex@users.noreply.github.com>
2022-05-02 18:35:36 -0700

    Create post.go
    Server side implementation for posting a new graph

---
+++ graph_shortest_distance/server/post.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/firebearrex/graph-shortest-distance-grpc-server-go/graph_shortest_distance/proto"
+)
+
+func (*Server) Post(ctx context.Context, in *pb.PostRequest) (*pb.PostResponse, error) {
+	log.Printf("Post was invoked with %v\n", in)
+
+	//totalVertices := in.TotalVertices
+	//edges := in.Edges
+	newGraph := Graph{
+		totalVertices: in.TotalVertices,
+		edges:         in.Edges,
+	}
+	currId := idHead
+	graphStore[idHead] = newGraph
+	idHead++
+
+	return &pb.PostResponse{Result: currId}, nil
+}
